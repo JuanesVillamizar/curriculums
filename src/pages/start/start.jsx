e4dc3945a11377182497af723effd6116fd621c8
Juanes <juanes1237@gmail.com>
2024-09-22 13:23:45 -0500

    Se termina el buscador de empleados en la pagina de inicio

--- src/pages/start/start.jsx
+++ src/pages/start/start.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, useRef } from "react";
 import CardWorker from "../../components/Card";
 import { WorkerContext } from "../../context/workersContext";
 
@@ -6,11 +6,14 @@ const Start = () => {
 
     const {getWorkers} = useContext(WorkerContext);
     const [workers, setWorkers] = useState([]);
+    const [workersAux, setWorkersAux] = useState([]);
+    let ref = useRef(null);
 
     useEffect(() => {
         let data = getWorkers();
-        // console.log(data);
+        console.log(data);
         setWorkers(data);
+        setWorkersAux(data);
     }, []);
 
     let generateColor = () => {
@@ -22,11 +25,41 @@ const Start = () => {
         return `#${colorHex.padStart(6, '0')}`;
     }
 
+    const _handleChangeSearch = (input) => {
+        // console.log(input.target.value);
+        if(input.target.value == ''){
+            setWorkers(workersAux);
+        }else{
+            let searchWorkers = [];
+            workersAux.map(e => {
+                console.log(e.nombre.toLowerCase(),input.target.value.toLowerCase());
+                if(e.nombre.toLowerCase().includes(input.target.value.toLowerCase())){
+                    console.log('Entra');
+                    searchWorkers.push(e);
+                }
+            });
+            // console.log(searchWorkers, workers, workersAux);
+            setWorkers(searchWorkers);
+        }
+    }
+
     return (
-        <div className="row" style={{ padding: '50px', display: 'flex', width: '100%', gap: 10, justifyContent: 'center' }}>
-            {
-                workers.map((worker, index) => <CardWorker className="col-4" worker={worker} key={index} color={generateColor()} />)
-            }
+        <div className="row">
+            <div className="col-12 my-3 px-5">
+                <div className="d-flex justify-content-between align-items-center">
+                    <span>Empleados</span>
+                    <div className="container-div-search">
+                        <input type="text" ref={ref} placeholder="Buscar..." className="input-search" onChange={_handleChangeSearch.bind(ref)}/>
+                    </div>
+                </div>
+            </div>
+            <div className="col-12">
+                <div className="row starts-container">
+                    {
+                        workers.map((worker, index) => <CardWorker className="col-4" worker={worker} key={index} color={generateColor()} />)
+                    }
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/worker/worker.jsx
+++ src/pages/worker/worker.jsx
@@ -4,17 +4,41 @@ import { WorkerContext } from "../../context/workersContext";
 
 
 export default function Worker() {
-  const {getWorkerById} = useContext(WorkerContext);
+  const {getWorkerById, getWorkers} = useContext(WorkerContext);
   const id = useParams().id;
   const [worker, setWorker] = useState([]);
+  let color = '#000';
 
   useEffect(() => {
-    let data = getWorkerById(id);
-    console.log(data, getWorkerById);
+    // let data = getWorkerById(id);
+    let dataWorkers = getWorkers();
+    let data = dataWorkers.filter(worker => worker.id == id)[0];
+    // console.log('Prueba -> ', dataWorkers, data);
+    color = generateColor();
     setWorker(data);
-  }, []);
+    // this.setState({});
+  }, [color]);
+
+  let generateColor = () => {
+    // Genera un número aleatorio entre 0 y 0xFFFFFF (16777215)
+    const color = Math.floor(Math.random() * 0xFFFFFF);
+    // Convierte el número a una cadena hexadecimal
+    const colorHex = color.toString(16);
+    // Asegura que la cadena tenga 6 caracteres (rellena con ceros si es necesario)
+    return `#${colorHex.padStart(6, '0')}`;
+  }
 
   return (
-    <div>worker</div>
-  )
+    <div style={{ width: '100%', display: 'flex', justifyContent: 'center', margin: '20px auto' }}>
+      
+      <div className="container-fluid" style={{ width: '80%', margin: 10, border: 'solid 1px #fefefe', borderRadius: '20px' }}>
+        <div className='container-img' style={{ backgroundColor: color }}>
+            <span>270 x 150</span>
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  );
 }
